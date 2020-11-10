7e1358a1d9609909732f8a8320e66b0b06b68f7e
Jung_dayoung <skswjdekdud@naver.com>
2020-11-11 00:02:42 +0900

    delete file test.py

--- Data_Science/인프런_파이썬입문과크롤링기초부트캠프_2020업데이트/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-res = requests.get('http://news.jtbc.joins.com/html/165/NB11978165.html')
-soup = BeautifulSoup(res.content, 'html.parser')
-mydata = soup.find('title')
-print(mydata.get_text())
